ef306c451fff80067862b33be1fe8fb31ca40178
Matthew Gall <me@matthewgall.com>
2020-04-18 15:19:22 +0100

    Improved metadata handling

--- public/app.js
+++ public/app.js
@@ -1,14 +1,15 @@
 function getRadio() {
-    $.getJSON( "//centova.isolationrad.io/external/rpc.php?m=streaminfo.get&username=isolation&charset=&mountpoint=&rid=isolation", function( data ) {
+    let ts = Math.floor(Date.now() / 1000)
+    $.getJSON( `//centova.isolationrad.io/external/rpc.php?m=streaminfo.get&username=isolation&charset=&mountpoint=&rid=isolation&ts=${ts}`, function( data ) {
         let track = data['data'][0]['track']
         
-        $('p#nowplaying').html(`<strong>Now Playing: </strong> ${track['artist']} with ${track['title']}`)
+        $('p#nowplaying').html(`<strong>Now Playing: </strong> ${track['artist']} - ${track['title']}`)
     })
 }
 
 $(function() {
     setInterval(function(){
         getRadio()
-    }, 20000);
+    }, 5000);
     getRadio();
 })
